9212b8ae797ed46866198dc41b0c3135660f1e62
Gao Yiping <70128007+pinguin247@users.noreply.github.com>
2026-05-20 21:17:13 +0800

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int high = nums.length - 1;
+        int low = 0;
+        int mid = 0;
+
+        while(low <= high){
+            mid = low + (high - low)/2;
+            if(nums[mid] > target){
+                high = mid - 1;
+            }else if(nums[mid] < target){
+                low = mid + 1;
+            }else{
+                return mid;
+            }
+        }
+        return -1; //if target not in nums
+    
+    }
+}
