802681e35d23c56d6b3e7585655980fc2e83e655
Gao Yiping <70128007+pinguin247@users.noreply.github.com>
2026-05-20 22:46:25 +0800

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+                //Optimized Solution - use a Hashmap
+        //3, 7-3 = 4 {3:0,
+        //4, 7-4 = 3 {4: 1}
+        int[] index = null;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if(map.containsKey(diff)){
+                return new int[]{map.get(diff), i};
+            }else{
+                map.put(nums[i], i);
+            }
+        }
+        return index;
+    }
+}
